284fdf9516c5fafe2b62785a1abeef16a8671e64
Fabio Kusaba <fabinhokb44@gmail.com>
2025-03-25 21:04:22 -0300

    criação das entidades Pedido e ItemPedido

---
+++ src/main/java/meu/curso/microservicos/pedido/model/Pedido.java
@@ -0,0 +1,50 @@
+package meu.curso.microservicos.pedido.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+
+    // Relacionamento -> um Pedido para vários ItemPedido
+    // mappedBy -> mapeado pela coluna "pedido" 
+    // cascade -> operações em cascata do relacionamento entre as tabelas
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+    private List<ItemPedido> itens;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemPedido> itens) {
+        this.itens = itens;
+    }
+
+}
