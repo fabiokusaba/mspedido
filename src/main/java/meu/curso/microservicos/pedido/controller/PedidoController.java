a3d46335e5dcb11aada0c7ea41832e31c39d7038
Fabio Kusaba <fabinhokb44@gmail.com>
2025-03-25 21:46:07 -0300

    enviando mensagens ao broker

--- src/main/java/meu/curso/microservicos/pedido/controller/PedidoController.java
+++ src/main/java/meu/curso/microservicos/pedido/controller/PedidoController.java
@@ -8,6 +8,8 @@ import meu.curso.microservicos.pedido.service.PedidoService;
 
 import java.util.List;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,17 +19,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/pedidos")
 public class PedidoController {
 
+    // Configuração RabbitMQ
+    private final RabbitTemplate rabbitTemplate;
+
     private final PedidoService pedidoService;
 
+    @Value("${broker.queue.processamento.name}")
+    private String routingKey;
+
     // Injeção de dependência através do construtor
-    public PedidoController(PedidoService pedidoService) {
+    public PedidoController(PedidoService pedidoService, RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
         this.pedidoService = pedidoService;
     }
 
     @PostMapping()
     public String criarPedido(@RequestBody Pedido pedido) {
-        pedidoService.salvarPedido(pedido);
-        return "Pedido salvo e enviado para processamento: " + pedido.getDescricao();
+        Pedido pedidoSalvo = pedidoService.salvarPedido(pedido);
+        rabbitTemplate.convertAndSend("", routingKey, pedidoSalvo.getDescricao());
+        return "Pedido salvo e enviado para processamento: " + pedidoSalvo.getDescricao();
     }
 
     @GetMapping
